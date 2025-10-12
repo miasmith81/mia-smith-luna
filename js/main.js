89c633fc7dde47197f937c38dc5a664eee088aef
miasmith81 <mismitha326@gmail.com>
2025-10-12 10:56:29 -0600

    Add action buttons for Luna Project and Art Institute links in artwork details

--- js/main.js
+++ js/main.js
@@ -492,17 +492,31 @@ class ArticService {
                     ` : ''}
                 </div>
                 
-                <a href="https://www.artic.edu/" 
-                   target="_blank" 
-                   rel="noopener noreferrer" 
-                   class="artic-link">
-                    Visit Art Institute of Chicago
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                        <polyline points="15 3 21 3 21 9"></polyline>
-                        <line x1="10" y1="14" x2="21" y2="3"></line>
-                    </svg>
-                </a>
+                <div class="artic-actions">
+                    <a href="https://miasmith81.github.io/Luna-Open-API-Project/" 
+                       target="_blank" 
+                       rel="noopener noreferrer" 
+                       class="artic-link luna-project-link">
+                        View Luna Project
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                            <polyline points="15 3 21 3 21 9"></polyline>
+                            <line x1="10" y1="14" x2="21" y2="3"></line>
+                        </svg>
+                    </a>
+                    
+                    <a href="https://www.artic.edu/" 
+                       target="_blank" 
+                       rel="noopener noreferrer" 
+                       class="artic-link">
+                        Visit Art Institute of Chicago
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                            <polyline points="15 3 21 3 21 9"></polyline>
+                            <line x1="10" y1="14" x2="21" y2="3"></line>
+                        </svg>
+                    </a>
+                </div>
             </div>
         `;
 
